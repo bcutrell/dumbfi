751cfffa5a494ac3f190063572f1f5194584fa62
Ben Cutrell <bcutrell@Apple-MacBook-Pro.local>
2024-06-20 16:52:08 -0400

    Adds skeleton for market data impl

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use clap::Parser;
-use dumbfi::{read_prices, run_backtest, Config, Context, Portfolio};
+use dumbfi::{run_backtest, Config, Context, MarketData, Portfolio};
 use std::fs;
 
 #[derive(Parser, Debug)]
@@ -16,13 +16,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_content = fs::read_to_string(cli.config)?;
     let config: Config = toml::from_str(&config_content)?;
 
-    let prices = read_prices(&config.prices_file)?;
+    let market_data = MarketData::new();
+    market_data.load_prices_from_csv(&config.prices_file)?;
 
     let portfolio = Portfolio::new(config.init_cash);
     let context = Context {
         config,
         portfolio,
-        prices,
+        market_data,
     };
     run_backtest(&context);
 
